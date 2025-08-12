3916a2b56411ef339464f4840a404b115dc7ac05
yodan <yodanbrandon@gmail.com>
2025-08-12 16:49:37 +0200

    save modif & prefab

--- Assets/_/Features/MovePlayer/Runtime/Move.cs
+++ Assets/_/Features/MovePlayer/Runtime/Move.cs
@@ -9,10 +9,15 @@ namespace MovePlayer.Runtime
         private Vector2 _move;
         private Rigidbody _rb;
 
-        [SerializeField] private float _speed;
+        [SerializeField] private float speed;
+        [SerializeField] private Collider zoneAttack;
 
+        private bool _onFrameAttack;
+        private float _timeToAttackFrame;
+        private float _time;
         private void Awake()
         {
+            zoneAttack.enabled = false;
             _rb = GetComponent<Rigidbody>();
             PlayerInput playerInput = GetComponent<PlayerInput>();
             foreach (var map in playerInput.actions.actionMaps)
@@ -26,12 +31,37 @@ namespace MovePlayer.Runtime
         private void Update()
         {
             Vector3 move = new(_move.x, 0f, _move.y);
-            _rb.MovePosition(transform.position + _speed * Time.deltaTime * move);
+            _rb.MovePosition(transform.position + speed * Time.deltaTime * move);
+            
+            if(_onFrameAttack)_timeToAttackFrame += Time.deltaTime;
+            
+            if(_timeToAttackFrame >= _time) AttackDisable();
         }
 
         public void MovePlayer(InputAction.CallbackContext context)
         {
             _move = context.ReadValue<Vector2>();
+        }
+
+        public void Attack(InputAction.CallbackContext context)
+        {
+            //animation coup de tete
+            if (context.started)
+            {
+                AttackEnable();
+                _onFrameAttack = true;
+            };
+            
+        }
+
+        public void AttackEnable()
+        {
+            zoneAttack.enabled = true;
+        }
+
+        public void AttackDisable()
+        {
+            zoneAttack.enabled = false;
         }
     }
 }
