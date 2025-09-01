36ef8ceabcfd8f08eb92c619f84778c20df12491
Lebrun Regis <lebrun.regis.formation@gmail.com>
2025-09-01 11:27:44 +0200

    Refined the attack system

--- Assets/_/Features/ActiveRagdoll/Runtime/Attack.cs
+++ Assets/_/Features/ActiveRagdoll/Runtime/Attack.cs
@@ -1,18 +1,19 @@
 using ActiveRagdoll.Runtime;
+using Grab.Runtime;
 using MovePlayer.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CameraRelativeMovement))]
+[RequireComponent(typeof(AnimatedProximityGrabber))]
 public class Attack : MonoBehaviour
 {
     [SerializeField] private Collider attackCollider;
     [SerializeField] private int damage;
-    [SerializeField] private float attackDuration = 1;
     private CameraRelativeMovement _movement;
     private Animator _animator;
     private AttackTrigger _attackTrigger;
-    public float movementLockDurationLeft = 0;
+    private AnimatedProximityGrabber _proximityGrabber;
     public float attackMoveSpeedMultiplier = 0.5f;
 
     private void Awake()
@@ -20,32 +21,32 @@ public class Attack : MonoBehaviour
         _movement = GetComponent<CameraRelativeMovement>();
         _animator = GetComponentInChildren<Animator>();
         _attackTrigger = GetComponentInChildren<AttackTrigger>();
+        _proximityGrabber = GetComponent<AnimatedProximityGrabber>();
 
         AttackAnimationEventListener animationEventListener = _animator.gameObject.AddComponent<AttackAnimationEventListener>();
-        animationEventListener.Initialize(_attackTrigger);
+        animationEventListener.Initialize(this,_animator, _attackTrigger);
     }
 
     public void PlayAttack(InputAction.CallbackContext context)
     {
-        if (context.performed && movementLockDurationLeft == 0)
+        if (context.performed && !_proximityGrabber.IsGrabbing())
         {
             _animator.SetBool("Attack", true);
-            _movement.moveSpeedMultiplier = attackMoveSpeedMultiplier;
-            movementLockDurationLeft = attackDuration;
+        }
+        else if (context.canceled)
+        {
+            _animator.SetBool("Attack", false);
         }
     }
 
-    private void Update()
+    public void SetSpeedMultiplier(bool isSet)
     {
-        if (movementLockDurationLeft > 0)
+        if (isSet)
         {
-            movementLockDurationLeft -= Time.deltaTime;
-            if (movementLockDurationLeft < 0)
-            {
-                movementLockDurationLeft = 0;
-                _animator.SetBool("Attack", false);
-                _movement.moveSpeedMultiplier = 1f;
-            }
+            _movement.moveSpeedMultiplier = attackMoveSpeedMultiplier;
+        }else
+        {
+            _movement.moveSpeedMultiplier = 1;
         }
     }
 }
