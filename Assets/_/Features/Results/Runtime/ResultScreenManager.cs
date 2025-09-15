b3bc62402558242e80d9408dd764042126ef9c7f
Lebrun Regis <lebrun.regis.formation@gmail.com>
2025-09-15 15:41:51 +0200

    Setup for end ui

---
+++ Assets/_/Features/Results/Runtime/ResultScreenManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Results.Runtime
+{
+    public class ResultScreenManager : MonoBehaviour
+    {
+        #region Publics
+        
+        #endregion
+        
+        
+        #region Unity Api
+        
+            // Start is called once before the first execution of Update after the MonoBehaviour is created
+            void Start()
+            {
+        
+            }
+        
+            // Update is called once per frame
+            void Update()
+            {
+        
+            }
+        
+        #endregion
+        
+        
+        #region Main Methods
+        
+        #endregion
+        
+        
+        #region Utils
+        
+        #endregion
+        
+        
+        #region Private and Protected
+        
+        #endregion
+        
+        
+    }
+}
---
+++ Assets/_/Features/UI/UI.Runtime/PlayerRank.cs
@@ -0,0 +1,45 @@
+using Animals.Data;
+using UnityEngine;
+
+namespace UI.Runtime
+{
+    public class PlayerRank : MonoBehaviour
+    {
+        #region Publics
+        public GameObject first;
+        public GameObject second;
+        public GameObject third;
+        public GameObject fourth;
+        #endregion
+
+
+        #region Unity Api
+
+        public void SetPlayerIcon(int rank)
+        {
+            first.SetActive(rank == 0);
+            second.SetActive(rank == 1);
+            third.SetActive(rank == 2);
+            fourth.SetActive(rank == 3);
+        }
+
+        #endregion
+
+
+        #region Main Methods
+
+        #endregion
+
+
+        #region Utils
+
+        #endregion
+
+
+        #region Private and Protected
+
+        #endregion
+
+
+    }
+}
