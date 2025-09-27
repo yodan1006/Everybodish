10abea8e6ba66658eeffac7f4eb2abd72496f4dd
yodan1006 <yodanbrandon@gmail.com>
2025-09-27 20:33:37 +0200

    vibration good

--- Assets/_/Features/StunSystem/Runtime/DamageReceiver.cs
+++ Assets/_/Features/StunSystem/Runtime/DamageReceiver.cs
@@ -1,6 +1,7 @@
 using MovePlayer.Runtime;
 using StunSystem.Runtime;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Stunsystem.Runtime
 {
@@ -8,12 +9,19 @@ namespace Stunsystem.Runtime
     public class DamageReceiver : MonoBehaviour
     {
         public float stunDuration = 5;
+        
         #region Publics
         public void TakeDamage(int damage)
         {
             Debug.Log("Took damage!");
             stat.HurtPlayer(damage);
+            if(hitEffect != null)
+                hitEffect.Play();
             animator.SetTrigger("Hit");
+            
+            // Déclencher la vibration de la manette
+            TriggerControllerVibration();
+            
             if (stat.CurrentLife() < 0)
             {
                 stun.StunForDuration(stunDuration);
@@ -22,9 +30,7 @@ namespace Stunsystem.Runtime
         }
         #endregion
 
-
         #region Unity Api
-
         private void Awake()
         {
             stat = GetComponent<PlayerStat>();
@@ -32,23 +38,52 @@ namespace Stunsystem.Runtime
         }
         #endregion
 
-
         #region Main Methods
+        private void TriggerControllerVibration()
+        {
+            // Obtenir la manette spécifique à ce joueur
+            if (Gamepad.all.Count > playerIndex)
+            {
+                var playerInput = GetComponentInParent<PlayerInput>();
+                if (playerInput != null && playerInput.devices.Count > 0)
+                {
+                    var gamepad = playerInput.devices[0] as Gamepad;
+                    if (gamepad != null)
+                    {
+                        gamepad.SetMotorSpeeds(vibrationIntensity, vibrationIntensity);
+                        Invoke(nameof(StopVibration), vibrationDuration);
+                    }
+                }
+
+            }
+        }
+
+        private void StopVibration()
+        {
+            if (Gamepad.all.Count > playerIndex)
+            {
+                var gamepad = Gamepad.all[playerIndex];
+                if (gamepad != null)
+                {
+                    gamepad.SetMotorSpeeds(0f, 0f);
+                }
+            }
+        }
 
         #endregion
-
 
         #region Utils
-
         #endregion
-
 
         #region Private and Protected
         [SerializeField] private Animator animator;
+        [SerializeField] private ParticleSystem hitEffect;
+        [SerializeField] private float vibrationDuration = 0.1f; // Durée de la vibration
+        [SerializeField] private float vibrationIntensity = 0.8f; // Intensité de la vibration (0-1)
         private PlayerStat stat;
         private Stun stun;
+        private int playerIndex = 0;
+
         #endregion
-
-
     }
 }
