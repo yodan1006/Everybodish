f6146063c2dd308d8ccc37b4dc27df8e696c2475
lebrunregis <145544209+lebrunregis@users.noreply.github.com>
2025-09-28 23:53:54 +0200

    refactoring

--- Assets/_/Features/ActiveRagdoll/Runtime/PlayerTeleporter.cs
+++ Assets/_/Features/ActiveRagdoll/Runtime/PlayerTeleporter.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 namespace ActiveRagdoll.Runtime
 {
@@ -13,16 +12,15 @@ namespace ActiveRagdoll.Runtime
         [SerializeField] private GameObject m_ragdollRoot;
         //Rigidbody placed on the player character root
         private Rigidbody _rootRigidBody;
-        private CharacterController _characterController;
         private Animator _animator;
         private void Awake()
         {
             _rootRigidBody = m_playerRoot.GetComponent<Rigidbody>();
-            _characterController = m_playerRoot.GetComponentInChildren<CharacterController>();
             _animator = m_playerRoot.GetComponentInChildren<Animator>();
         }
         public void TeleportTo(Transform transform)
         {
+            _animator.enabled = false;
             m_playerRoot.SetActive(false);
             m_ragdollRoot.SetActive(false);
             m_playerRoot.transform.SetPositionAndRotation(transform.position, transform.rotation);
@@ -31,34 +29,17 @@ namespace ActiveRagdoll.Runtime
             m_ragdollRoot.transform.localRotation = configurableJointExtended.InitialLocalRotation;
             m_playerRoot.SetActive(true);
             m_ragdollRoot.SetActive(true);
+            _animator.enabled = true;
         }
-
 
         public void ReconnectCharacterControllerToRagdoll()
         {
-            _animator.enabled = false;
+            m_ragdollRoot.transform.GetPositionAndRotation(out Vector3 ragdollPos, out Quaternion ragdollRot);
             ConfigurableJoint configurableJoint = m_ragdollRoot.AddComponent<ConfigurableJoint>();
             ConfigurableJointExtended configurableJointExtended = m_ragdollRoot.GetComponent<ConfigurableJointExtended>();
-
-            // 1. Cache ragdoll's current world pos & rot
-            m_ragdollRoot.transform.GetPositionAndRotation(out Vector3 ragdollPos, out Quaternion ragdollRot);
-
-            // 2. Move player root to ragdoll’s world position (player controller need to keep upright)
-            _characterController.enabled = false; // disable first so it doesn’t block teleport
-            _characterController.transform.position = ragdollPos;
-            _characterController.enabled = true;
-
-            // 3. Reset ragdoll relative to player
-            m_ragdollRoot.transform.position = m_playerHip.transform.position;
-            m_ragdollRoot.transform.localRotation = configurableJointExtended.InitialLocalRotation;
-
-            // 4. Reconnect rigidbody + joints
+            TeleportTo(m_playerRoot.transform);
             configurableJointExtended.Reconnect(_rootRigidBody, configurableJoint, m_playerHip);
-
-            // 5. Restore ragdoll’s world pos & rot after joint reconnect
-            m_ragdollRoot.transform.rotation = ragdollRot;
-
-            _animator.enabled = true;
+            m_ragdollRoot.transform.SetPositionAndRotation(ragdollPos, ragdollRot);
         }
     }
 }
