7332f11f5fc83d376f86e87eb58a7ce7dc5e0ec6
lebrunregis <145544209+lebrunregis@users.noreply.github.com>
2025-09-29 00:13:51 +0200

    Mostly working out of stun behavior

--- Assets/_/Features/ActiveRagdoll/Runtime/PlayerTeleporter.cs
+++ Assets/_/Features/ActiveRagdoll/Runtime/PlayerTeleporter.cs
@@ -18,15 +18,24 @@ namespace ActiveRagdoll.Runtime
             _rootRigidBody = m_playerRoot.GetComponent<Rigidbody>();
             _animator = m_playerRoot.GetComponentInChildren<Animator>();
         }
-        public void TeleportTo(Transform transform)
+        public void TeleportTo(Transform target)
         {
+            // 1. Cache target position and rotation
+            Vector3 targetPos = target.position;
+            Quaternion targetRot = target.rotation;
+
+            // 2. Deactivate player and ragdoll to avoid physics issues
             _animator.enabled = false;
             m_playerRoot.SetActive(false);
             m_ragdollRoot.SetActive(false);
-            m_playerRoot.transform.SetPositionAndRotation(transform.position, transform.rotation);
-            ConfigurableJointExtended configurableJointExtended = m_ragdollRoot.GetComponent<ConfigurableJointExtended>();
-            m_ragdollRoot.transform.localPosition = configurableJointExtended.InitialLocalPosition;
-            m_ragdollRoot.transform.localRotation = configurableJointExtended.InitialLocalRotation;
+
+            // 3. Move player root
+            m_playerRoot.transform.SetPositionAndRotation(targetPos, targetRot);
+
+            // 4. Move ragdoll root to match player root in world space
+            m_ragdollRoot.transform.SetPositionAndRotation(m_playerHip.transform.position, targetRot);
+
+            // 6. Reactivate objects
             m_playerRoot.SetActive(true);
             m_ragdollRoot.SetActive(true);
             _animator.enabled = true;
@@ -34,12 +43,37 @@ namespace ActiveRagdoll.Runtime
 
         public void ReconnectCharacterControllerToRagdoll()
         {
-            m_ragdollRoot.transform.GetPositionAndRotation(out Vector3 ragdollPos, out Quaternion ragdollRot);
-            ConfigurableJoint configurableJoint = m_ragdollRoot.AddComponent<ConfigurableJoint>();
+            // 1. Cache the ragdoll's current world position and rotation
+            Vector3 ragdollPos = m_ragdollRoot.transform.position;
+            Quaternion ragdollRot = m_ragdollRoot.transform.rotation;
+
+            // 2. Deactivate objects to prevent physics interference
+            _animator.enabled = false;
+            m_playerRoot.SetActive(false);
+            m_ragdollRoot.SetActive(false);
+
+            // 3. Teleport player root to ragdoll's current position
+            m_playerRoot.transform.SetPositionAndRotation(ragdollPos, ragdollRot);
+
+            // 4. Reset ragdoll to initial pose at the same position
             ConfigurableJointExtended configurableJointExtended = m_ragdollRoot.GetComponent<ConfigurableJointExtended>();
-            TeleportTo(m_playerRoot.transform);
-            configurableJointExtended.Reconnect(_rootRigidBody, configurableJoint, m_playerHip);
             m_ragdollRoot.transform.SetPositionAndRotation(ragdollPos, ragdollRot);
+
+            // 5. Reconnect the configurable joint
+            ConfigurableJoint configurableJoint = m_ragdollRoot.GetComponent<ConfigurableJoint>();
+            if (configurableJoint == null)
+            {
+                configurableJoint = m_ragdollRoot.AddComponent<ConfigurableJoint>();
+            }
+            configurableJointExtended.Reconnect(_rootRigidBody, configurableJoint, m_playerHip);
+            //TODO: why does it disable itself?
+            configurableJointExtended.enabled = true;
+
+            // 6. Reactivate objects
+            m_playerRoot.SetActive(true);
+            m_ragdollRoot.SetActive(true);
+            _animator.enabled = true;
         }
+
     }
 }
